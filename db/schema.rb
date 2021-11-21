17c7c81900b94c86a1c26f87c9806422d11568bd
kellyhan <kjhan@uchicago.edu>
2021-11-21 21:49:00 +0000

    added username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_21_195857) do
+ActiveRecord::Schema.define(version: 2021_11_21_214440) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_11_21_195857) do
     t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
   end
 
 end
