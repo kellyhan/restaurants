ea27c6541d35535f121cf2dd56216f820fad9cd3
kellyhan <kjhan@uchicago.edu>
2021-11-21 19:56:51 +0000

    user sign up stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,31 @@
 Rails.application.routes.draw do
+  
 
+# Routes for the User account:
 
+  # SIGN UP FORM
+  get("/user_sign_up", { :controller => "users", :action => "sign_up_form" })        
+  # CREATE RECORD
+  post("/insert_user", { :controller => "users", :action => "create"  })
+      
+  # EDIT PROFILE FORM        
+  get("/edit_user_profile", { :controller => "users", :action => "edit_profile_form" })       
+  # UPDATE RECORD
+  post("/modify_user", { :controller => "users", :action => "update" })
+  
+  # DELETE RECORD
+  get("/cancel_user_account", { :controller => "users", :action => "destroy" })
+
+  # ------------------------------
+
+  # SIGN IN FORM
+  get("/user_sign_in", { :controller => "users", :action => "sign_in_form" })
+  # AUTHENTICATE AND STORE COOKIE
+  post("/user_verify_credentials", { :controller => "users", :action => "create_cookie" })
+  
+  # SIGN OUT        
+  get("/user_sign_out", { :controller => "users", :action => "destroy_cookies" })
+             
+  #------------------------------
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,13 @@
 
 ActiveRecord::Schema.define(version: 0) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+  
 end
