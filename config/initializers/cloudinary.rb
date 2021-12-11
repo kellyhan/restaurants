6cf04cf01a7cae029fdf8c5b1d7e5095b6bbed62
kellyhan <kjhan@uchicago.edu>
2021-12-11 19:20:14 +0000

    heroku 16

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = ENV["kelly-appdev"]
+  config.api_key = ENV["257822941918745"]
+  config.api_secret = ENV["257822941918745"]
+  config.cdn_subdomain = true
+end
